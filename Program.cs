f03cfb57743ff81bda290523a6124a9f3763fb42
Alexandr <sanekstroyka33@mail.ru>
2023-06-16 10:54:03 +0300

    первая домашка

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿// Console.Write( "Введите первое число " );
+// int number1 = int.Parse(Console.ReadLine()!);
+
+// Console.Write("Введите второе число " );
+// int number2 = int.Parse(Console.ReadLine()!);
+// if(number1 > number2)
+// {
+//     Console.WriteLine($"{number1} max ");
+// }
+// else
+// {
+//      Console.WriteLine($"{number2} max ");
+// }
+
+// Console.Write( "Введите первое число " );
+// int a = int.Parse(Console.ReadLine()!);
+// Console.Write( "Введите второе число " );
+// int b = int.Parse(Console.ReadLine()!);
+// Console.Write( "Введите третее число " );
+// int c = int.Parse(Console.ReadLine()!);
+// int max = a;
+// if(max < b)
+// {
+//     max = b;
+// }
+// if(max < c)
+// {
+//     max = c;
+// }
+//  Console.WriteLine($"{ max} ");
+
+// Console.Write( "Введите число " );
+// int num0 = int.Parse(Console.ReadLine()!);
+// if(num0 % 2 == 0)
+// {
+//         Console.WriteLine("чётное");
+// }
+// else
+// {
+//     Console.WriteLine("нечётное");
+// }
+Console.Write( "Введите число N " );
+int N = int.Parse(Console.ReadLine()!); 
+int fff = 1;
+ while (fff <= N)
+ {if( fff % 2 == 0)
+    
+     Console.Write($"{fff} "); 
+     fff += 1; 
+ }
+ 
